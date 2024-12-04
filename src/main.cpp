88afaef917953108a705b7a2830261ad40c34856
droooney <d.vor7@mail.ru>
2024-12-04 22:15:28 +0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+#ifndef DELAY
+#define DELAY 10000
+#endif
+
+Servo servo;
+
+int currentAngle = random(0, 89);
+
+void setup() {
+  servo.attach(4);
+  Serial.begin(9600);
+}
+
+void loop() {
+  currentAngle = currentAngle < 90 ? currentAngle + 90 : currentAngle - 90;
+
+  servo.write(currentAngle);
+  delay(DELAY);
+}
